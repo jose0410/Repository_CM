3710bb4625e6b1b5ddb32972d9e17257ba4c14dd
= <=>
2018-02-26 23:18:43 -0500

    Dish structure is done

--- Labs_CM/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr01_20181/labscm20181/views/Dish.java
+++ Labs_CM/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr01_20181/labscm20181/views/Dish.java
@@ -2,6 +2,7 @@ package co.edu.udea.compumovil.gr01_20181.labscm20181.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import co.edu.udea.compumovil.gr01_20181.labscm20181.R;
 
@@ -11,5 +12,8 @@ public class Dish extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dish);
+    }
+
+    public void onCheckboxClicked(View view) {
     }
 }
