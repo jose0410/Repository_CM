27cdd87431ee525a45faed45665762d804851ce0
LuisErnesto <ernesto.monsalve@udea.edu.co>
2018-02-27 19:30:42 -0500

    Splash Screen

--- Labs_CM/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr01_20181/labscm20181/views/MainActivity.java
+++ Labs_CM/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr01_20181/labscm20181/views/MainActivity.java
@@ -1,24 +1,36 @@
 package co.edu.udea.compumovil.gr01_20181.labscm20181.views;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.github.clans.fab.FloatingActionMenu;
 import com.github.clans.fab.FloatingActionButton;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import co.edu.udea.compumovil.gr01_20181.labscm20181.R;
+import co.edu.udea.compumovil.gr01_20181.labscm20181.SplashActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private FloatingActionMenu materialDesignFAM;
     private FloatingActionButton fabDish, fabDrink;
+    long Delay = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
 
         materialDesignFAM = findViewById(R.id.request);
         fabDish = findViewById(R.id.addDish);
@@ -41,6 +53,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 intent = new Intent(this,Drink.class);
                 startActivity(intent);
                 break;
+
         }
     }
 }
